43bcb224f729d70b749210ac862b8c3e69087e55
Kamil Pomykała <kpomykala2002@gmail.com>
2024-06-18 20:50:27 +0200

    fix: Windows after Linux fuckup

--- build.rs
+++ build.rs
@@ -4,6 +4,6 @@ extern crate windres;
 fn main() {
 	#[cfg(windows)]
 	if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-    	Build::new().compile("tray.rc").unwrap();
+    	windres::Build::new().compile("tray.rc").unwrap();
 	}
 }
--- src/tray.rs
+++ src/tray.rs
@@ -9,15 +9,9 @@ enum Message {
 }
 
 pub fn init_tray() {
-    let icon = if cfg!(windows) {
-        get_icon_windows()
-    } else {
-        get_icon_unix()
-    };
-
     let mut tray = TrayItem::new(
         "Knob Rocker",
-        icon,
+        IconSource::Resource("tray-default"),
     ).unwrap();
 
     #[cfg(windows)]
@@ -38,24 +32,6 @@ pub fn init_tray() {
     wait_for_input(rx);
 }
 
-fn get_icon_windows() -> IconSource {
-    IconSource::Resource("tray-default")
-}
-
-fn get_icon_unix() -> IconSource {
-    let cursor_red = Cursor::new(include_bytes!("../icons/icon.png"));
-    let decoder_red = png::Decoder::new(cursor_red);
-    let mut reader = decoder_red.read_info().unwrap();
-    let buffer_size = reader.output_buffer_size();
-    let mut buf_red = vec![0; buffer_size];
-    reader.next_frame(&mut buf_red).unwrap();
-
-    IconSource::Data {
-        data: buf_red,
-        height: 32,
-        width: 32,
-    }
-}
 
 fn wait_for_input(receiver: Receiver<Message>) {
     loop {
